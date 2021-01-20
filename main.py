492cc8b050b9a09c613a74a006628db1fba7bc0a
ilyashenko.kv <email@email.org>
2021-01-20 19:41:30 +0400

    Разработали API для приложения. Реализовали обработку парамтров командной строки.

--- main.py
+++ main.py
@@ -2,3 +2,35 @@
 Главный модуль программы
 Точка старта
 """
+import loader
+import click
+
+
+BUBBLE = 'bubble'
+INSERT = 'insert'
+SELECTION = 'selection'
+
+
+@click.command()
+@click.option('--filename', default=None,
+              help='Имя файла с несортированными данными')
+@click.option('--algorithm', default=BUBBLE,
+              help='Алгоритм сортировки.')
+def sorter(filename, algorithm):
+    """ Простая утилита для сортировки чисел
+    """
+    print(filename, algorithm)
+    if filename is None:
+        unsorted_data = loader.load_from_input()
+    else:
+        unsorted_data = loader.load_from_file(filename)
+
+    allowed_algorithms = [BUBBLE, INSERT, SELECTION]
+    if algorithm not in allowed_algorithms:
+        print('Неправильно введено имя алгоритма')
+        print('Правильные варианты:', allowed_algorithms)
+        exit(1)
+
+
+if __name__ == '__main__':
+    sorter()
--- sorting.py
+++ sorting.py
@@ -1,3 +1,24 @@
 """
 Функции сортировки
 """
+
+
+def bubble_sort(unsorted_data: list) -> list:
+    """Сортировка методом пузырька
+    :param unsorted_data Несортированные данные
+    :return Сортированные данные
+    """
+
+
+def insert_sort(unsorted_data: list) -> list:
+    """Сортировка методом вставки
+    :param unsorted_data Несортированные данные
+    :return Сортированные данные
+    """
+
+
+def selection_sort(unsorted_data: list) -> list:
+    """Сортировка методом выбора
+    :param unsorted_data Несортированные данные
+    :return Сортированные данные
+    """
